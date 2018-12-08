6304d06797fc679f835e5f99eac11940cd33336f
Christie L Knudson <christie.knudson@Christies-MacBook-Pro.local>
2018-12-08 17:53:15 -0600

    Fixed all errors and added fortunes

--- src/main.js
+++ src/main.js
@@ -4,18 +4,18 @@ const page3 = document.querySelector('#page3');
 const boxText1 = document.querySelectorAll('#page1 div h2');
 const boxText2 = document.querySelectorAll('#page2 div h2');
 const boxText3 = document.querySelectorAll('#page3 div h2');
+const yourFortune = document.querySelector('#pagefortune');
 
 
 
 
-//change "top"
+
 const topBlock = document.querySelectorAll('.top-block');
 const bottom = document.querySelectorAll('.bottom');
 const left = document.querySelectorAll('.left');
 const right = document.querySelectorAll('.right');
 const firstFunctionIndexNumbers = [0,1,4,5,8,9,12,13,16,17,20,21,24,25];
 const upDown = () => {
-	//for loop for each top and bottom
 	for(let i = 0; i < top.length; i++) {
 		topBlock[i].classList.toggle('transition-up');
 	};
@@ -65,17 +65,17 @@ function wrapper() {
 
 
 function wrapper2() {
-	const moveChange2 = () => {
-		boxText2.classList.toggle('blue');
-	};
-
 	const loopThrough2 = () => {
 		let thisNumber = this.childNodes[0].nodeValue;
 		let numberValue = Number(thisNumber);
 
 		for (let i = 0; i < numberValue * 2; i++) {
 			setTimeout(function() {
-				moveChange2();
+				if (firstFunctionIndexNumbers.includes(i)) {
+					upDown();
+				} else {
+					leftRight();
+				}
 			}, i * 500);
 		}
 		setTimeout(function() {
@@ -94,24 +94,25 @@ function wrapper2() {
 
 
 function wrapper3() {
-	const moveChange3 = () => {
-		boxText3.classList.toggle('blue');
-	};
-
 	const loopThrough3 = () => {
 		let thisNumber = this.childNodes[0].nodeValue;
 		let numberValue = Number(thisNumber);
 
 		for (let i = 0; i < numberValue * 2; i++) {
 			setTimeout(function() {
-				moveChange3();
+				if (firstFunctionIndexNumbers.includes(i)) {
+					upDown();
+				} else {
+					leftRight();
+				}
 			}, i * 500);
 		}
 		setTimeout(function() {
 			page3.classList.add('hide');
 			page3.classList.remove('show');
-//			pageFortune.classList.add('show');
-//			showFortune();
+			yourFortune.classList.remove('hide');
+			yourFortune.classList.add('show');
+			showFortune();
 		}, numberValue * 1000);
 	};
 	loopThrough3();
@@ -120,27 +121,35 @@ function wrapper3() {
 
 
 
-//const fortunes = ['1','2','3'];
-//const yourFortune = document.querySelector('#pagefortune');
+const fortunes = ['It is certain.','It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
 //const fortuneTextPlaceholder = document.createTextNode('');
 //yourFortune.appendChild(fortuneTextPlaceholder);
 
-//const showFortune = () => {
-//	fortunes.sort(function(a, b){return 0.5 - Math.random()});
-//	const fortuneText = document.createTextNode(fortunes[0]);
-//	yourFortune.appendChild(fortuneText);
-//	yourFortune.classList.add('fortune-show');
-//};
+const showFortune = () => {
+	fortunes.sort(function(a, b){return 0.5 - Math.random()});
+	const fortuneText = document.createTextNode(fortunes[0]);
+	yourFortune.appendChild(fortuneText);
+	yourFortune.classList.add('fortune-show');
+};
 
 
 
 
-//double check the boxText1.length
+//create an event listener for each one?
 for(let i = 0; i < boxText1.length; i++) {
 	boxText1[i].addEventListener('click', wrapper);
 //	boxText2[i].addEventListener('click', wrapper2);
 //	boxText3[i].addEventListener('click', wrapper3);
 };
+
+for(let i = 0; i < boxText2.length; i++) {
+	boxText2[i].addEventListener('click', wrapper2);
+};
+
+for(let i = 0; i < boxText3.length; i++) {
+	boxText3[i].addEventListener('click', wrapper3);
+};
+
 
 
 
