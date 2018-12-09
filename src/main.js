20ff683902bd537481e5cbee991f6de1784e526d
Christie L Knudson <christie.knudson@Christies-MacBook-Pro.local>
2018-12-08 18:12:49 -0600

    Add concerning comments to js file to fix later

--- src/main.js
+++ src/main.js
@@ -54,7 +54,7 @@ function wrapper() {
 			page2.classList.remove('hide');
 			page2.classList.add('show');
 		}, thisBoxLength * 600);
-		//play around with the timing
+		//times between this and animation - wish to be smoother
 	};
 	loopThrough();
 };
@@ -124,13 +124,14 @@ function wrapper3() {
 const fortunes = ['It is certain.','It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
 //const fortuneTextPlaceholder = document.createTextNode('');
 //yourFortune.appendChild(fortuneTextPlaceholder);
+//is this necessary? doesn't seem so.
 
 const showFortune = () => {
 	fortunes.sort(function(a, b){return 0.5 - Math.random()});
 	const fortuneText = document.createTextNode(fortunes[0]);
 	yourFortune.appendChild(fortuneText);
 //	yourFortune.classList.add('fortune-show');
-// 	class not showing?
+// 	class animation above not showing?
 };
 
 
