c60e38b179eb1f0069b3e24e2c9bc08e36b8786a
Christie L Knudson <christie.knudson@Christies-MacBook-Pro.local>
2018-12-10 13:38:42 -0600

    Add const varibles for individual boxes to pair with animations

--- src/main.js
+++ src/main.js
@@ -11,6 +11,11 @@ const boxText3 = document.querySelectorAll('#page3 div');
 
 const firstFunctionIndexNumbers = [0,1,4,5,8,9,12,13,16,17,20,21,24,25];
 
+const topBlock = document.querySelectorAll('.top-block');
+const bottom = document.querySelectorAll('.bottom');
+const left = document.querySelectorAll('.left');
+const right = document.querySelectorAll('.right');
+
 const upDown = () => {
 	for(let i = 0; i < topBlock.length; i++) {
 		topBlock[i].classList.toggle('transition-up');
