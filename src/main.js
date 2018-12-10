3e5298795b3fa3fce650b5c4ba6a75a0629493e4
Christie L Knudson <christie.knudson@Christies-MBP.EAP1750H>
2018-12-09 18:01:45 -0600

    Fix all concerns from last email

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 const page1 = document.querySelector('#page1');
 const page2 = document.querySelector('#page2');
 const page3 = document.querySelector('#page3');
-const boxText1 = document.querySelectorAll('#page1 div h2');
-const boxText2 = document.querySelectorAll('#page2 div h2');
-const boxText3 = document.querySelectorAll('#page3 div h2');
+//const boxText1 = document.querySelectorAll('#page1 div h2');
+//const boxText2 = document.querySelectorAll('#page2 div h2');
+//const boxText3 = document.querySelectorAll('#page3 div h2');
 const yourFortune = document.querySelector('#pagefortune');
 
-
-
-
+//Clickable box area
+const boxText = document.getElementsByClassName('box');
+const boxText1 = document.querySelectorAll('#page1 div');
+const boxText2 = document.querySelectorAll('#page2 div');
+const boxText3 = document.querySelectorAll('#page3 div');
 
 const topBlock = document.querySelectorAll('.top-block');
 const bottom = document.querySelectorAll('.bottom');
@@ -16,7 +18,7 @@ const left = document.querySelectorAll('.left');
 const right = document.querySelectorAll('.right');
 const firstFunctionIndexNumbers = [0,1,4,5,8,9,12,13,16,17,20,21,24,25];
 const upDown = () => {
-	for(let i = 0; i < top.length; i++) {
+	for(let i = 0; i < topBlock.length; i++) {
 		topBlock[i].classList.toggle('transition-up');
 	};
 	for(let i = 0; i < bottom.length; i++) {
@@ -37,7 +39,9 @@ const leftRight = () => {
 
 function wrapper() {
 	const loopThrough = () => {
-		let thisBoxText = this.childNodes[0].nodeValue;
+//		let thisBoxText = this.childNodes[0].nodeValue;
+//		let thisBoxLength = thisBoxText.length;
+		let thisBoxText = this.childNodes[0].innerHTML;
 		let thisBoxLength = thisBoxText.length;
 
 		for (let i = 0; i < thisBoxLength * 2; i++) {
@@ -47,14 +51,13 @@ function wrapper() {
 				} else {
 					leftRight();
 				}
-			}, i * 300);
+			}, i * 400);
 		}
 		setTimeout(function() {
 			page1.classList.add('hide');
 			page2.classList.remove('hide');
 			page2.classList.add('show');
-		}, thisBoxLength * 600);
-		//times between this and animation - wish to be smoother
+		}, thisBoxLength * 800);
 	};
 	loopThrough();
 };
@@ -66,7 +69,7 @@ function wrapper() {
 
 function wrapper2() {
 	const loopThrough2 = () => {
-		let thisNumber = this.childNodes[0].nodeValue;
+		let thisNumber = this.childNodes[0].innerHTML;
 		let numberValue = Number(thisNumber);
 
 		for (let i = 0; i < numberValue * 2; i++) {
@@ -76,14 +79,14 @@ function wrapper2() {
 				} else {
 					leftRight();
 				}
-			}, i * 300);
+			}, i * 400);
 		}
 		setTimeout(function() {
 			page2.classList.add('hide');
 			page2.classList.remove('show');
 			page3.classList.remove('hide');
 			page3.classList.add('show');
-		}, numberValue * 600);
+		}, numberValue * 800);
 	};
 	loopThrough2();
 };
@@ -95,7 +98,7 @@ function wrapper2() {
 
 function wrapper3() {
 	const loopThrough3 = () => {
-		let thisNumber = this.childNodes[0].nodeValue;
+		let thisNumber = this.childNodes[0].innerHTML;
 		let numberValue = Number(thisNumber);
 
 		for (let i = 0; i < numberValue * 2; i++) {
@@ -105,7 +108,7 @@ function wrapper3() {
 				} else {
 					leftRight();
 				}
-			}, i * 300);
+			}, i * 400);
 		}
 		setTimeout(function() {
 			page3.classList.add('hide');
@@ -113,7 +116,7 @@ function wrapper3() {
 			yourFortune.classList.remove('hide');
 			yourFortune.classList.add('show');
 			showFortune();
-		}, numberValue * 600);
+		}, numberValue * 800);
 	};
 	loopThrough3();
 };
@@ -122,26 +125,17 @@ function wrapper3() {
 
 
 const fortunes = ['It is certain.','It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
-//const fortuneTextPlaceholder = document.createTextNode('');
-//yourFortune.appendChild(fortuneTextPlaceholder);
-//is this necessary? doesn't seem so.
-
 const showFortune = () => {
 	fortunes.sort(function(a, b){return 0.5 - Math.random()});
 	const fortuneText = document.createTextNode(fortunes[0]);
 	yourFortune.appendChild(fortuneText);
-//	yourFortune.classList.add('fortune-show');
-// 	class animation above not showing?
 };
 
 
 
 
-//create an event listener for each one?
 for(let i = 0; i < boxText1.length; i++) {
 	boxText1[i].addEventListener('click', wrapper);
-//	boxText2[i].addEventListener('click', wrapper2);
-//	boxText3[i].addEventListener('click', wrapper3);
 };
 
 for(let i = 0; i < boxText2.length; i++) {
@@ -151,7 +145,4 @@ for(let i = 0; i < boxText2.length; i++) {
 for(let i = 0; i < boxText3.length; i++) {
 	boxText3[i].addEventListener('click', wrapper3);
 };
-
-
-
 
