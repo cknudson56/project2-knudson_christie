497e9430f12329113d6d0e79203ad4f6cf8f11df
Christie L Knudson <christie.knudson@Christies-MacBook-Pro.local>
2018-12-10 13:35:28 -0600

    Add animation functions to js

--- src/main.js
+++ src/main.js
@@ -11,20 +11,35 @@ const boxText3 = document.querySelectorAll('#page3 div');
 
 const firstFunctionIndexNumbers = [0,1,4,5,8,9,12,13,16,17,20,21,24,25];
 
+const upDown = () => {
+	for(let i = 0; i < topBlock.length; i++) {
+		topBlock[i].classList.toggle('');
+	};
+	for(let i = 0; i < bottom.length; i++) {
+		bottom[i].classList.toggle('');
+	};
+};
+const leftRight = () => {
+	for(let i = 0; i < left.length; i++) {
+		left[i].classList.toggle('');
+	};
+	for(let i = 0; i < right.length; i++) {
+		right[i].classList.toggle('');
+	};
+};
 
 function wrapper() {
 	const loopThrough = () => {
 		let thisBoxText = this.childNodes[0].innerHTML;
 		let thisBoxLength = thisBoxText.length;
 
-		for(let i = 0; i < thisBoxLength * 2; i++) {
+		for (let i = 0; i < thisBoxLength * 2; i++) {
 			setTimeout(function() {
 				if (firstFunctionIndexNumbers.includes(i)) {
-
+					upDown();
 				} else {
-
+					leftRight();
 				}
-				
 			}, i * 500);
 		}
 	};
