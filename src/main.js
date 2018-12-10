3edfd664cc84f5455ff63bff6c2d8a29e4730e2a
Christie L Knudson <christie.knudson@Christies-MacBook-Pro.local>
2018-12-10 13:51:32 -0600

    Add fortune function to js

--- src/main.js
+++ src/main.js
@@ -99,11 +99,18 @@ function wrapper3() {
 			page3.classList.remove('show');
 			yourFortune.classList.remove('hide');
 			yourFortune.classList.add('show');
+			showFortune();
 		}, numberValue * 1000);
 	};
 	loopThrough3();
 };
 
+const fortunes = ['It is certain.','It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
+const showFortune = () => {
+	fortunes.sort(function(a, b){return 0.5 - Math.random()});
+	const fortuneText = document.createTextNode(fortunes[0]);
+	yourFortune.appendChild(fortuneText);
+};
 
 for(let i = 0; i < boxText1.length; i++) {
 	boxText1[i].addEventListener('click', wrapper);
