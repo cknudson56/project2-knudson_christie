0368fe3f083f6961aab88a5b8eaaefc0eeea2871
Christie L Knudson <christie.knudson@Christies-MacBook-Pro.local>
2018-12-10 13:21:36 -0600

    Add initial const variables to js

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const page1 = document.querySelector('#page1');
+const page2 = document.querySelector('#page2');
+const page3 = document.querySelector('#page3');
+const yourFortune = document.querySelector('#pagefortune');
+
+//Clickable box area
+const boxText = document.getElementsByClassName('box');
+const boxText1 = document.querySelectorAll('#page1 div');
+const boxText2 = document.querySelectorAll('#page2 div');
+const boxText3 = document.querySelectorAll('#page3 div');
+
